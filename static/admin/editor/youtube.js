518ca6778854a3ace2ff494c5c43428bf19be0f1
sebousan <sebousan@gmail.com>
2022-10-04 13:43:03 +0200

    add admin to theme

---
+++ static/admin/editor/youtube.js
@@ -0,0 +1,21 @@
+CMS.registerEditorComponent({
+    id: "youtube",
+    label: "Youtube",
+    fields: [{
+        name: "id",
+        label: "Youtube Video ID",
+        widget: "string"
+    }],
+    pattern: /{{< youtube\s+(?<id>[A-Za-z0-9\-]+)\s+>}}/,
+    fromBlock: function(match) {
+        return {
+            id: match[1],
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< youtube ${obj.id} >}}`;
+    },
+    toPreview: function(obj) {
+        return `<img src="https://i3.ytimg.com/vi/${obj.id}/hqdefault.jpg" alt="Youtube Video"/>`;
+    },
+});
