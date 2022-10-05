bea231f7c3dc7df04a57b4a223f12a7d4634ea5d
sebousan <sebousan@gmail.com>
2022-10-05 14:44:50 +0200

    author

--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -1,9 +1,9 @@
 // import Alert from 'bootstrap/js/src/alert';
 // import Carousel from 'bootstrap/js/src/carousel';
-import 'bootstrap/js/src/collapse';
+// import 'bootstrap/js/src/collapse';
 // import 'bootstrap/js/src/dropdown';
 // import 'bootstrap/js/src/modal';
-// import Offcanvas from 'bootstrap/js/src/offcanvas';
+import Offcanvas from 'bootstrap/js/src/offcanvas';
 // import Popover from 'bootstrap/js/src/popover';
 // import 'bootstrap/js/src/scrollspy';
 // import Tab from 'bootstrap/js/src/tab';
