7c984f2104cf299040bf989802be7f2c4b21efc5
sebousan <sebousan@gmail.com>
2022-12-22 16:07:39 +0100

    test

--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 // import 'bootstrap/js/src/alert';
 // import 'bootstrap/js/src/carousel';
 // import 'bootstrap/js/src/collapse';
-import 'bootstrap/js/src/dropdown';
+// import 'bootstrap/js/src/dropdown';
 // import 'bootstrap/js/src/modal';
-import 'bootstrap/js/src/offcanvas';
+// import 'bootstrap/js/src/offcanvas';
 // import 'bootstrap/js/src/popover';
 // import 'bootstrap/js/src/scrollspy';
 // import 'bootstrap/js/src/tab';
