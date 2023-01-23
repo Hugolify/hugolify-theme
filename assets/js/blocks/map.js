929fb3d3157aecf2836669595c12bdb804c523f3
sebousan <sebousan@gmail.com>
2023-01-23 19:56:19 +0100

    add map marker

--- assets/js/blocks/map.js
+++ assets/js/blocks/map.js
@@ -37,19 +37,32 @@ class BlockMap {
         let location = JSON.parse(this.map.dataset.location),
             coordinate = location.coordinates.reverse(),
             zoom = JSON.parse(this.map.dataset.zoom),
-            map = L.map(this.map, {
-                center: coordinate, 
-                zoom: zoom,
-                scrollWheelZoom: false
-            });
+            newIcon = false,
+            map = false;
+
+        // options
+        map = L.map(this.map, {
+            center: coordinate, 
+            zoom: zoom,
+            scrollWheelZoom: false
+        });
 
         // add tiles
+        // https://leafletjs.com/reference.html#tilelayer
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
+        // new icon
+        // https://leafletjs.com/reference.html#icon
+        newIcon = L.icon({
+            iconUrl: '/assets/images/map-marker.svg',
+            iconSize:     [50, 50],
+            iconAnchor:   [25, 40]
+        });
+
         // add marker to map
-        L.marker(coordinate).addTo(map);
+        L.marker(coordinate, {icon: newIcon}).addTo(map);
     }
 }
 
