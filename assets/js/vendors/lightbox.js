f8a69d17b39f7c71caa89350252526a40ed59ec0
sebousan <sebousan@gmail.com>
2023-01-26 15:25:15 +0100

    add caption to lightbox

--- assets/js/vendors/lightbox.js
+++ assets/js/vendors/lightbox.js
@@ -1,5 +1,7 @@
 import Tobii from '@midzer/tobii'
 const tobii = new Tobii({
+    captionsSelector: "self",
+    captionAttribute: "data-caption",
     zoom: false,
     navLabel: ["Image précédente", "Image suivante"],
     closeLabel: "Fermer"
