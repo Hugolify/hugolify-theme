284ffcf0af603ca3375be371e1616370cffe92b6
sebousan <sebousan@gmail.com>
2023-01-26 18:08:59 +0100

    i18n lightbox

--- assets/js/vendors/lightbox.js
+++ assets/js/vendors/lightbox.js
@@ -1,8 +1,11 @@
 import Tobii from '@midzer/tobii'
+const closeLabel = window.i18n.close
+const nextLabel =window.i18n.next
+const previousLabel = window.i18n.previous
 const tobii = new Tobii({
     captionsSelector: "self",
     captionAttribute: "data-caption",
     zoom: false,
-    navLabel: ["Image précédente", "Image suivante"],
-    closeLabel: "Fermer"
+    navLabel: [previousLabel, nextLabel],
+    closeLabel: closeLabel
 })
