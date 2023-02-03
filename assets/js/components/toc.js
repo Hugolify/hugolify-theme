4d8267b02a13792f5abf59cec2e035b8f5ce81b7
sebousan <sebousan@gmail.com>
2023-02-03 09:51:00 +0100

    js toc fix

--- assets/js/components/toc.js
+++ assets/js/components/toc.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     /* hide offCanvasMenu when clicking an anchors */
-    var myOffcanvas = document.getElementById("toc"),
-        bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas),
-        menuLinks = myOffcanvas.querySelectorAll("a");
-    console.log(bsOffcanvas);
-    for (var i = 0; i < menuLinks.length; i++) {
-        menuLinks[i].addEventListener("click", function (event) {
-            bsOffcanvas.hide();
-        });
+    const myOffcanvas = document.getElementById("toc");
+    if (myOffcanvas) {
+        const bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+        const menuLinks = myOffcanvas.querySelectorAll("a");
+        for (var i = 0; i < menuLinks.length; i++) {
+            menuLinks[i].addEventListener("click", function (event) {
+                bsOffcanvas.hide();
+            });
+        }
     }
 });
   
