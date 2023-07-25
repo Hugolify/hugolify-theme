c3d4e5ae02f8972b6ed727de6e63b2c892617c11
sebousan <sebousan@gmail.com>
2023-07-25 15:06:11 +0200

    TOC: fix closing offcanvas

--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -1,14 +1,14 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
 
-// import 'js/bootstrap/src/alert';
-// import 'js/bootstrap/src/carousel';
-import 'js/bootstrap/src/collapse';
-import 'js/bootstrap/src/dropdown';
-import 'js/bootstrap/src/modal';
-import 'js/bootstrap/src/offcanvas';
-// import 'js/bootstrap/src/popover';
-// import 'js/bootstrap/src/scrollspy';
-// import 'js/bootstrap/src/tab';
-// import 'js/bootstrap/src/toast';
-// import 'js/bootstrap/src/tooltip';
+// import Alert from 'js/bootstrap/src/alert';
+// import Carousel from 'js/bootstrap/src/carousel';
+import Collapse from 'js/bootstrap/src/collapse';
+import Dropdown from 'js/bootstrap/src/dropdown';
+import Modal from 'js/bootstrap/src/modal';
+import Offcanvas from 'js/bootstrap/src/offcanvas';
+// import Popover from 'js/bootstrap/src/popover';
+// import Scrollspy from 'js/bootstrap/src/scrollspy';
+// import Tab from 'js/bootstrap/src/tab';
+// import Toast from 'js/bootstrap/src/toast';
+// import Tooltip from 'js/bootstrap/src/tooltip';
