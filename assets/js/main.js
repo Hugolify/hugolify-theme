f40c5c424554a833dd4fea25e5240bdfb2a83008
arthaud-proust <arthaudp33@gmail.com>
2023-12-07 14:38:43 +0100

    feat: notes with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,8 @@ import './components/carousel';
 import './components/gallery';
 import './components/gauge';
 import './components/map';
+import './components/notes';
 import './components/stickyNav';
 import './components/toc';
+import './components/tooltip';
 import './components/video';
