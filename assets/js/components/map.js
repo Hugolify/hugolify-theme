7f76265b1a01837deef6d3ce51b9ec71e7db5b26
sebousan <sebousan@gmail.com>
2025-01-17 14:56:41 +0100

    fix map

--- assets/js/components/map.js
+++ assets/js/components/map.js
@@ -111,8 +111,15 @@ class Map {
         tile: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors &copy; CARTO'
+      },
+      // Stadiamaps Alidade smooth
+      {
+        tile: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+        attribution:
+          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }
     ];
+    console.log(tiles);
     L.tileLayer(tiles[this.tileSelect].tile, {
       attribution: tiles[this.tileSelect].attribution
     }).addTo(this.map);
