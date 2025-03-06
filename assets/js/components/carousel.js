e9b5850a6f942350cb10c3c16ce77a569bd9b92a
sebousan <sebousan@gmail.com>
2025-03-06 18:20:24 +0100

    add Splidejs in static folder

--- assets/js/components/carousel.js
+++ assets/js/components/carousel.js
@@ -19,15 +19,13 @@ class Carousel {
     // JS
     this.splideJS = document.createElement('script');
     this.splideJS.type = 'text/javascript';
-    this.splideJS.src =
-      'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js';
+    this.splideJS.src = '/assets/js/splide.min.js';
     document.getElementsByTagName('body')[0].appendChild(this.splideJS);
 
     // CSS
     this.splideCSS = document.createElement('link');
     this.splideCSS.rel = 'stylesheet';
-    this.splideCSS.href =
-      'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css';
+    this.splideCSS.href = '/assets/css/splide.min.css';
     document.getElementsByTagName('head')[0].appendChild(this.splideCSS);
 
     this.splideJS.addEventListener('load', () => {
