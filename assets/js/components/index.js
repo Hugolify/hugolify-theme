7dc60bf7be8d60ce59228d31a06b4c95e80317e5
sebousan <sebousan@gmail.com>
2026-01-08 17:37:38 +0100

    feature: add video-embed and youtube API

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -1,3 +1,4 @@
 import './menu';
 import './toc';
 import './video';
+import './youtube';
---
+++ assets/js/components/youtube.js
@@ -0,0 +1,51 @@
+// 1. Load the YouTube IFrame Player API code asynchronously
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 2. This function creates an <iframe> (and YouTube player) after the API code downloads
+window.onYouTubeIframeAPIReady = function() {
+    var playerElements = document.querySelectorAll('.js-youtube-player');
+
+    if (playerElements.length === 0) {
+        return;
+    }
+
+    playerElements.forEach(function(playerElement) {
+        // Extract the video ID from the "data-video-id" attribute
+        var videoId = playerElement.dataset.videoId;
+        var videoAutoplay = playerElement.dataset.videoAutoplay === undefined ? 1 : parseInt(playerElement.dataset.videoAutoplay);
+        var videoControls = playerElement.dataset.videoControls === undefined ? 0 : parseInt(playerElement.dataset.videoControls);
+
+        new YT.Player(playerElement, {
+            videoId: videoId, // Insert the extracted ID here
+            host: 'https://www.youtube-nocookie.com',
+            playerVars: {
+                'autoplay': videoAutoplay,   // Auto-play the video
+                'controls': videoControls,   // Hide player controls
+                'showinfo': 0,               // Hide video title and info
+                'modestbranding': 1,         // Minimize YouTube logo
+                'loop': 1,                   // Loop the video
+                'fs': 0,                     // Hide fullscreen button
+                'cc_load_policy': 0,         // Hide closed captions
+                'iv_load_policy': 3,         // Hide annotations
+                'autohide': 0,               // Auto-hide controls
+                'playlist': videoId          // REQUIRED: The video ID is needed here again to loop a single video
+            },
+            events: {
+                'onReady': function(event) {
+                    onPlayerReady(event, videoAutoplay);
+                }
+            }
+        });
+    });
+}
+
+// 3. The API will call this function when the video player is ready
+function onPlayerReady(event, autoplay) {
+    if (autoplay) {
+        // CRITICAL: Mute the video to allow autoplay on modern browsers (Chrome, Safari, etc.)
+        event.target.mute();
+    }
+}
